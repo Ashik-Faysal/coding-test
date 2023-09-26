c6ab2ff7347b34e16d115e46fc5ca9c3802ba25d
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-09-26 21:18:11 +0600

    added login page

--- script/script.js
+++ script/script.js
@@ -1,8 +1,26 @@
-// write your JavaScript here
 const mobileMenu = document.getElementById("mobileMenu");
 const humbergerMenu = document.getElementById("menu");
+const loginButton = document.getElementById("log-in");
+const loginContainer = document.getElementById("login-container");
+const loginForm = document.getElementById("login-form");
 
 humbergerMenu.addEventListener("click", () => {
   humbergerMenu.classList.toggle("fa-xmark");
   mobileMenu.classList.toggle("active");
+});
+
+loginButton.addEventListener("click", () => {
+  // Toggle the visibility of the login container when the "Log In" button is clicked
+  loginContainer.classList.toggle("show-login");
+
+  // Add or remove a class on the body to hide background content
+  document.body.classList.toggle("hide-background");
+});
+
+// If you want to close the login form when clicking outside of it
+document.addEventListener("click", (event) => {
+  if (!loginContainer.contains(event.target) && event.target !== loginButton) {
+    loginContainer.classList.remove("show-login");
+    document.body.classList.remove("hide-background");
+  }
 });
